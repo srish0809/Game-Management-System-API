c27ad69e7239fbfd0a1b66b9a44b6374efbef3db
Srashti Soni <srashisoni2001.ss@gmail.com>
2024-07-07 16:06:45 +0530

    added soft delete feature

--- models/game.model.js
+++ models/game.model.js
@@ -1,10 +1,11 @@
-const prisma = require('../config/db.config');
+const prisma = require("../config/db.config");
 
 const createGame = (data) => prisma.game.create({ data });
-const getAllGames = () => prisma.game.findMany();
+const getAllGames = () => prisma.game.findMany({ where: { deleted: false } });
 const getGameById = (id) => prisma.game.findUnique({ where: { id } });
 const updateGame = (id, data) => prisma.game.update({ where: { id }, data });
-const deleteGame = (id) => prisma.game.delete({ where: { id } });
+const deleteGame = (id) =>
+  prisma.game.delete({ where: { id }, data: { deleted: true } });
 
 module.exports = {
   createGame,
